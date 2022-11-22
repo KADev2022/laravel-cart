dfecfa2041bf1995b5ad732b396e5051bd6c544a
Kazi Arham <kaziarham2003@outlook.com>
2022-11-22 10:12:22 +0000

    Saving products to session

--- laravel-cart/app/Http/Controllers/CartController.php
+++ laravel-cart/app/Http/Controllers/CartController.php
@@ -3,11 +3,38 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class CartController extends Controller
 {
     public function cart() 
     {
+        dd(session('cartItems'));
         return view('cart.cart');
+    }
+
+    public function addToCart($id)
+    {
+        $product = Product::findOrFail($id);
+        $cartItems = session()->get('cartItems', []);
+
+        /*
+        Check if a product already exists and needs to be increased 
+        or product does not exist in the session so session needs to created
+        */
+        if (isset($cartItems[$id])) {
+            $cartItems[$id]['quantity']++;
+        } else {
+            $cartItems[$id] = [
+                "image_path" => $product->image_path,
+                "name" => $product->name,
+                "details" => $product->details,
+                "price" => $product->price,
+                "quantity" => 1
+            ];
+        }
+
+        session()->put('cartItems', $cartItems);
+        return redirect()->back()->with('success', 'Product added to cart!');
     }
 }
